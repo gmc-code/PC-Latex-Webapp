43fc6cc5b9d6d2495031f5709b266c7639188da3
gmc-code <gmccarthy@tpg.com.au>
2025-09-26 21:02:33 +1000

    js coords

---
+++ app/static/js/limit-points.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const numInputs = document.querySelectorAll('input[name="num_points"]');
+
+    numInputs.forEach(numInput => {
+        const min = parseInt(numInput.min);
+        const max = parseInt(numInput.max);
+
+        numInput.addEventListener('input', function () {
+            let val = parseInt(this.value);
+            if (!isNaN(val)) {
+                if (val < min) this.value = min;
+                if (val > max) this.value = max;
+            }
+        });
+    });
+});
