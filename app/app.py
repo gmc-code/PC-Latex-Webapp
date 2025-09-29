aba4b6ef20cbb244eeaf67eaaaf3575b2a1438b0
gmc-code <gmccarthy@tpg.com.au>
2025-09-29 16:43:32 +1000

    tri

--- app/app.py
+++ app/app.py
@@ -1315,38 +1315,38 @@ def area_of_a_triangle_create():
     return send_file(file, as_attachment=True, mimetype=mimetype)
 
 
-##########################################################################
-# area of a rectangle
-##########################################################################
+# ##########################################################################
+# # area of a rectangle
+# ##########################################################################
 
 
-@app.route("/area_of_a_rectangle")
-def area_of_a_rectangle():
-    return render_template(
-        "genform_tqcbf.html",
-        title="Area of a Rectangle",
-        link="/area_of_a_rectangle_create",
-        num_per_page="4",
-        min_questions="1",
-        max_questions="20",
-        img_filename="area_of_a_rectangle.png",
-        pdf_filename="area_of_a_rectangle.pdf",
-        title_text="Area of a Rectangle",
-        checkbox_text="Show Dimension Lines",
-    )
+# @app.route("/area_of_a_rectangle")
+# def area_of_a_rectangle():
+#     return render_template(
+#         "genform_tqcbf.html",
+#         title="Area of a Rectangle",
+#         link="/area_of_a_rectangle_create",
+#         num_per_page="4",
+#         min_questions="1",
+#         max_questions="20",
+#         img_filename="area_of_a_rectangle.png",
+#         pdf_filename="area_of_a_rectangle.pdf",
+#         title_text="Area of a Rectangle",
+#         checkbox_text="Show Dimension Lines",
+#     )
 
 
-@app.route("/area_of_a_rectangle_create", methods=["POST"])
-def area_of_a_rectangle_create():
-    # Safely parse and clamp numq
-    numq = parse_and_clamp(request.form, "numq", 4, 1, 20, cast_type=int)
-    #
-    show_dimension_lines_bool = request.form.get("checkbox1") == "on"
-    title_text = request.form.get("title_text", "")
-    file_type = request.form.get("file_type", "pdf")
-    mimetype = {"zip": "application/zip", "pdf": "application/pdf"}.get(file_type, "application/pdf")
-    file = arearect.create_booklet_area_of_a_rectangle(numq, title_text, file_type=file_type, show_dimension_lines_bool=show_dimension_lines_bool)
-    return send_file(file, as_attachment=True, mimetype=mimetype)
+# @app.route("/area_of_a_rectangle_create", methods=["POST"])
+# def area_of_a_rectangle_create():
+#     # Safely parse and clamp numq
+#     numq = parse_and_clamp(request.form, "numq", 4, 1, 20, cast_type=int)
+#     #
+#     show_dimension_lines_bool = request.form.get("checkbox1") == "on"
+#     title_text = request.form.get("title_text", "")
+#     file_type = request.form.get("file_type", "pdf")
+#     mimetype = {"zip": "application/zip", "pdf": "application/pdf"}.get(file_type, "application/pdf")
+#     file = arearect.create_booklet_area_of_a_rectangle(numq, title_text, file_type=file_type, show_dimension_lines_bool=show_dimension_lines_bool)
+#     return send_file(file, as_attachment=True, mimetype=mimetype)
 
 
 
