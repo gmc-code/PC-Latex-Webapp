67a76b96ae1a6e518ac922831987ae2cb6d038e6
gmc-code <gmccarthy@tpg.com.au>
2025-10-01 20:10:33 +1000

    cc

--- app/latex/area_of_a_circle/area_of_a_circle_functions.py
+++ app/latex/area_of_a_circle/area_of_a_circle_functions.py
@@ -31,7 +31,7 @@ def get_area_of_a_circle_dict(radius=None, rotation=None, show_dimension_lines_b
     if rotation is None:
         rotation = get_rotations_shuffled()[0]
 
-    draw_radius = round(random.uniform(0, 1.0) + 1.3, 3)
+    draw_radius = round(random.uniform(0, 1.8) + 1.3, 3)
     calc_radius_value = radius
     calcarea_value = round(math.pi * radius ** 2,3)
 
